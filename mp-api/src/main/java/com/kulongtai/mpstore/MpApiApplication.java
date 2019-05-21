09713481640330488f4f9b62874fcd4c86fd89fa
qq57694878 <57694878@qq.com>
2019-05-21 22:42:16 +0800

    init

---
+++ mp-api/src/main/java/com/kulongtai/mpstore/MpApiApplication.java
@@ -0,0 +1,13 @@
+package com.kulongtai.mpstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpApiApplication.class, args);
+	}
+
+}
