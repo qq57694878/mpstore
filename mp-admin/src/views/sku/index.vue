5e88a8ee0cc0b1f37c355fd4b9f0fa99fcc76f07
lijinliang_hx <jinliang.li@dhc.com.cn>
2019-06-04 20:54:37 +0800

    调整结构

--- mp-admin/src/views/sku/index.vue
+++ mp-admin/src/views/sku/index.vue
@@ -7,7 +7,7 @@
                         <div class="widget-title am-cf">出售中的商品</div>
                     </div>
                     <div class="widget-body am-fr">
-                        <div class="am-u-sm-12 am-u-md-6 am-u-lg-6">
+                        <div class="am-u-sm-12 am-u-md-4 am-u-lg-4">
                             <div class="am-form-group">
                                 <div class="am-btn-toolbar">
                                     <div class="am-btn-group am-btn-group-xs">
@@ -18,6 +18,25 @@
                                     </div>
                                 </div>
                             </div>
+                        </div>
+                        <div class="am-u-sm-12 am-u-md-8 am-u-lg-8">
+                            <div class="am-u-sm-12 am-u-md-3">
+                                <div class="am-form-group">
+                                    <select data-am-selected="{btnSize: 'sm'}">
+                                        <option  value="">所有状态</option>
+                                        <option v-for="item in skuStatusOptions" :value="item.value">{{item.label}}</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="am-u-sm-12 am-u-md-3">
+                                <div class="am-form-group">
+                                    <input type="text" class="am-form-field" :value="query.skuName">
+                                    <span class="am-input-group-btn">
+                                         <button class="am-btn  am-btn-default am-btn-success tpl-am-btn-success am-icon-search" type="button"></button>
+                                     </span>
+                                </div>
+                            </div>
+
                         </div>
                         <div class="am-scrollable-horizontal am-u-sm-12">
                             <table width="100%" class="am-table am-table-compact am-table-striped
@@ -52,7 +71,7 @@
                                     <td class="am-text-middle">
                                             <span :class="$item['skuStatus'] == 1 ? 'x-color-green'
                                                 : 'x-color-red'">
-                                            {{ $item['skuStatus']|code2value('sku_status') }}
+                                            {{ $item['skuStatus'] | code2value('sku_status') }}
                                             </span>
                                     </td>
                                     <td class="am-text-middle">{{ $item['createTime'] | moment('YYYY-MM-DD HH:mm:ss')}}
@@ -81,13 +100,13 @@
                                 <el-pagination
                                         @size-change="handleSizeChange"
                                         @current-change="handleCurrentChange"
-                                        :current-page="page.current"
+                                        :current-page="query.current"
                                         :page-sizes="[10, 20, 50, 100]"
-                                        :page-size="page.size"
+                                        :page-size="query.size"
                                         layout="total, sizes, prev, pager, next, jumper"
                                         :total="total">
                                 </el-pagination>
-                        </div>
+                            </div>
 
                         </div>
                     </div>
@@ -100,18 +119,23 @@
 
 </style>
 <script>
-import {getSkuList} from '@/api/sku'
-    export default{
-        data(){
+    import {getSkuList} from '@/api/sku'
+    import {type2options} from '@/util/codeTable'
+    export default {
+        data() {
             return {
-                loading:false,
+                skuStatusOptions:type2options("sku_status"),
+                loading: false,
                 msg: 'hello vue',
-                page:{
-                  current:1,
-                    size:10,
+                query: {
+                    current: 1,
+                    size: 10,
+                    skuName:"",
+                    skuStatus:""
                 },
-                total:0,
-                skuList: [
+                total: 0,
+                skuList:[],
+             /*   skuList: [
                     {
                         skuName: "保养机油套餐",
                         createTime: new Date().getTime(),
@@ -130,21 +154,21 @@ import {getSkuList} from '@/api/sku'
                         skuId: 100002,
                         mainUrl: "http://www.kulongtai.com/2.jpg"
                     }
-                ]
+                ]*/
             }
         },
-        created(){
+        created() {
             this.handleList();
         },
         components: {},
         methods: {
             handleSizeChange(val) {
-                this.page.size=val;
+                this.page.size = val;
                 console.log(`每页 ${val} 条`);
                 this.handleList();
             },
             handleCurrentChange(val) {
-                this.page.current=val;
+                this.page.current = val;
                 console.log(`当前页: ${val}`);
                 this.handleList();
             },
@@ -153,16 +177,16 @@ import {getSkuList} from '@/api/sku'
              * @detail 赋值为tableData表格即可
              *
              **/
-            handleList () {
-                console.log(this.page);
-                this.loading=true;
-                getSkuList(this.page)
+            handleList() {
+                console.log(this.query);
+                this.loading = true;
+                getSkuList(this.query)
                     .then(res => {
                         const data = res.data.data;
                         setTimeout(() => {
                             this.skuList = data.records;
                             this.total = data.total;
-                            this.loading=false;
+                            this.loading = false;
                         }, 1000);
                     });
             }
