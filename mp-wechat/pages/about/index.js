f7900dbf88f363f81b9d8c8d1d742be3ad8b8ad5
qq57694878 <57694878@qq.com>
2019-06-16 23:45:12 +0800

    代码修改

---
+++ mp-wechat/pages/about/index.js
@@ -0,0 +1,80 @@
+let App = getApp(),
+  wxParse = require("../../wxParse/wxParse.js");
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  this.getAbout();
+  },
+
+  /**
+   * 获取商品信息
+   */
+  getAbout() {
+    let _this = this;
+    App._get('/mpapi/about/getAbout', {
+    }).then(function (result) {
+      // 富文本转码
+      if (data.detail.content.length > 0) {
+        wxParse.wxParse('content', 'html', data.content, _this, 0);
+      }
+    });
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
